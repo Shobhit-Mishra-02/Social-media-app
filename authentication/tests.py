d72b32057e4e0b9ec0abb1a37e29203ab1c64014
shobhit <shobhitmishra2002@gmail.com>
2023-11-25 21:50:56 +0530

    added some test in authentication

--- authentication/tests.py
+++ authentication/tests.py
@@ -6,9 +6,20 @@ from django.contrib.auth import get_user_model
 class AccountUserManagerTests(TestCase):
     
     def test_create_user(self):
+        USER_EMAIL = "normal@user.com"
+        PASSWORD = "foo"
+
         User = get_user_model()
-        user = User.objects.create_user(email="normal@user.com", password="foo")
-        self.assertEqual(user.email, "normal@user.com")
+        user = User.objects.create_user(email=USER_EMAIL, password=PASSWORD)
+        
+        self.assertEqual(user.email, USER_EMAIL)
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+        self.assertEqual(user.last_name, "")
+
+
         self.assertTrue(user.is_active)
+        self.assertTrue(user.check_password(PASSWORD))
+
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
