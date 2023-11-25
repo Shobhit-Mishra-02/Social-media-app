d40fae9319f091f695e21c7642760692a57093e8
shobhit <shobhitmishra2002@gmail.com>
2023-11-25 12:31:09 +0530

    added the authorization procedure during the loing

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 def login(request):
     if request.method == "POST":
         email = request.POST["email"]
         password = request.POST["password"]
+
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+            # user is verified
+            print(f"The user is {user.email}")
+        else:
+            # user is not verified
+            print("authentication fails")
+            
 
         return redirect("/")
     
