211844fe8f4ee5752279d1130a1643243539a1c6
shobhit <shobhitmishra2002@gmail.com>
2023-11-25 22:00:47 +0530

    added home app and linked to project

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication.apps.AuthenticationConfig'
+    'authentication.apps.AuthenticationConfig',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("authentication.urls"))
+    path("", include("authentication.urls")),
+    path("social/", include("home.urls")),
 ]
