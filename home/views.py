9af3bc8a9cbc1f867fe197741628bc5a58ac8ec2
shobhit <shobhitmishra2002@gmail.com>
2023-12-01 21:00:37 +0530

    added signout option in the navbar

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 def index(request):
+    if not request.user.is_authenticated:
+        # redirecting to login page
+        return redirect("/")
     
     return render(request, "home/index.html")
