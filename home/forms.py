884ab174521560c84999f1224aae013a03aa1196
shobhit <shobhitmishra2002@gmail.com>
2023-12-10 18:56:17 +0530

    added code for the form design

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
 class PostCreationForm(forms.Form):
-    title = forms.CharField(max_length=300)
-    content = forms.CharField(widget=forms.Textarea)
     
-    image = forms.ImageField(required=False)
-    caption = forms.CharField(max_length=400, required=False)
+    title = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'class':'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5', 'rows':'5'}))
+    
+    image = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class':'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+    
+    caption = forms.CharField(max_length=400, required=False, widget=forms.TextInput(attrs={'class':'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
