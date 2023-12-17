890e6305fe888ef16e9c0ec1f63a4dcc37c85682
shobhit <shobhitmishra2002@gmail.com>
2023-12-17 19:44:57 +0530

    added changes in get_posts controller

--- ajax/views.py
+++ ajax/views.py
@@ -7,14 +7,41 @@ from home.models import Post
 from .serializers import PostModelSerializer
 
 
+'''
+REQUEST
+The front part will send the body with 'request_page' value and it contains the requested page number
+post_body = {
+    requested_page: 1
+}
+
+RESPONSE
+After getting the requested page number the required data is generated like this
+response = {
+    total_pages:2,
+    page_number: 1,
+    post_number: 3,
+    posts:[
+    {title, content, created_at, image, caption, email, username},
+    {title, content, created_at, image, caption, email, username},
+    {title, content, created_at, image, caption, email, username},
+    ]
+}
+'''
+
 @csrf_exempt
 @login_required
 def get_posts(request):
 
     if request.method == "POST":
+        page_numer = request.POST['requested_page']
         
-        # user_posts = request.user.post_set.all()
-        posts = Post.objects.all().order_by('-created_at')
+        end_index = page_numer*3
+        start_index = end_index - 3
+
+        number_of_posts = Post.objects.all().count()
+
+        posts = Post.objects.all().order_by('-created_at')[start_index:end_index]
+
         serialized = PostModelSerializer(posts, many=True)
         serialized_posts = serialized.data
 
